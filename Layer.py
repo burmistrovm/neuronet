a2c2b617218a4f8e4f8a3d236b88b5e074a722d6
burmistrovm <burmistrowm@mail.ru>
2017-12-23 22:37:59 +0300

    Many layers prediction added

--- Layer.py
+++ Layer.py
@@ -5,7 +5,9 @@ class Layer:
     #_weigt[i][j] is weight between j input and i neuron of layer
 
     def __init__(self, input_dim, units, activation='logistic', eta = 0.1):
-        self._weights = np.random.uniform(-1, 1, [2, 3]) # bias is _weights[i][0]
+        self.input_dim = input_dim
+        self.units = units
+        self._weights = np.random.uniform(-1, 1, [units, input_dim + 1]) # bias is _weights[i][0]
         self._eta = eta #eta must be computed in learning loop
 
     # def fit(self, X, target, n_epoch = 1):
@@ -17,11 +19,12 @@ class Layer:
     #         prediction = 1 / (1 + np.exp(-z))
     #         self._weights = self._weights + self._eta * (target[i] - prediction) * X[i]
 
-    def predict(self, X, target):
-        obj_num = X.shape[0]
-        obj_size = X.shape[1]
-        
-        X = np.insert(X, 0, 1, axis = 1)
-        z = self._weights[i].dot(X[i])
+    def predict(self, X):
+        if len(X.shape) == 1:
+            X = np.insert(X, 0, 1) # first element will be multuplied on bias
+        else:
+            X = np.insert(X, 0, 1, axis = 1) # it works with many objects
+
+        z = self._weights.dot(X.T) # rows of weights matrix are neurons, cols of X - features
         prediction = 1 / (1 + np.exp(-z))
         return prediction
--- Network.py
+++ Network.py
@@ -1,1 +1,29 @@
-class Network:
+import numpy as np
+from Layer import Layer
+
+
+class Network(object):
+    # network has any layers and manages fiting and predicting
+
+    def __init__(self, input_dim, eta = 0.1):
+        self._input_dim = input_dim
+        self._layers = np.array([])
+        self._learning_rate = eta # must be dynamical
+
+    def add_layer(self, units, activation = 'logistic'):
+        self._layers = np.array([])
+        if self._layers.size == 0:
+            new_layer = Layer(input_dim = self._input_dim, units = units, activation = activation)
+        else:
+            new_layer_dim = self._layers[self._layers.size - 1].input_dim
+            new_layer = Layer(input_dim = new_layer_dim, units = units, activation = activation)
+
+        self._layers = np.append(self._layers, new_layer)
+
+    def delete_last_layer(self):
+        self._layers = np.delete(self._layers, self._layers.size - 1, 0)
+        
+    def predict(self, X):
+        for layer in self._layers:
+            X = layer.predict(X)
+        return X
