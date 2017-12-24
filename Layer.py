7d66bb14e5162fcb451b43b5bccaa33beb3a1cfd
burmistrovm <burmistrowm@mail.ru>
2017-12-24 06:22:24 +0300

    Backprop

--- Layer.py
+++ Layer.py
@@ -10,21 +10,17 @@ class Layer:
         self._weights = np.random.uniform(-1, 1, [units, input_dim + 1]) # bias is _weights[i][0]
         self._eta = eta #eta must be computed in learning loop
 
-    # def fit(self, X, target, n_epoch = 1):
-    #     obj_num = X.shape[0]
-    #     obj_size = X.shape[1]
-    #     X = np.insert(X, 0, 1, axis = 1)
-    #     for i in range(0, obj_num): # must be n_epoch iterations
-    #         z = self._weights[i].dot(X[i])
-    #         prediction = 1 / (1 + np.exp(-z))
-    #         self._weights = self._weights + self._eta * (target[i] - prediction) * X[i]
-
+    def correct_weights(self, errors, last_predict, last_input):
+        dsigma = last_predict * (1 - last_predict)
+        dw =  errors * dsigma
+        last_layer_errors = dw.dot(self._weights)
+        last_input = np.insert(last_input, 0, 1)
+        for i in range(len(dw)):
+            self._weights[i] = self._weights[i] - last_input * dw[i] * self._eta
+        return last_layer_errors
+        
     def predict(self, X):
-        if len(X.shape) == 1:
-            X = np.insert(X, 0, 1) # first element will be multuplied on bias
-        else:
-            X = np.insert(X, 0, 1, axis = 1) # it works with many objects
-
-        z = self._weights.dot(X.T) # rows of weights matrix are neurons, cols of X - features
+        X = np.insert(X, 0, 1) # first element will be multuplied on bias
+        z = self._weights.dot(X) # rows of weights matrix are neurons, cols of X - features
         prediction = 1 / (1 + np.exp(-z))
         return prediction
--- Network.py
+++ Network.py
@@ -7,23 +7,48 @@ class Network(object):
 
     def __init__(self, input_dim, eta = 0.1):
         self._input_dim = input_dim
+        self._output_dim = 0
+        self._num_layers = 0
         self._layers = np.array([])
         self._learning_rate = eta # must be dynamical
 
     def add_layer(self, units, activation = 'logistic'):
-        self._layers = np.array([])
         if self._layers.size == 0:
             new_layer = Layer(input_dim = self._input_dim, units = units, activation = activation)
         else:
-            new_layer_dim = self._layers[self._layers.size - 1].input_dim
+            new_layer_dim = self._layers[self._layers.size - 1].units
             new_layer = Layer(input_dim = new_layer_dim, units = units, activation = activation)
-
+        self._num_layers += 1
+        self._output_dim = units
         self._layers = np.append(self._layers, new_layer)
 
     def delete_last_layer(self):
+        self._num_layers -= 1
         self._layers = np.delete(self._layers, self._layers.size - 1, 0)
         
     def predict(self, X):
         for layer in self._layers:
             X = layer.predict(X)
         return X
+
+    def fit(self, X, target, n_epoch = 1):
+        if target.shape[1] != self._output_dim:
+            raise ValueError("Size of outer layer must be equal to size of target vector")
+
+        obj_num = X.shape[0]
+        #obj_size = X.shape[1]
+        for i in range(0, obj_num ):
+            current_object = X[i]
+            inputs = []
+            outputs = []
+            for layer in self._layers: # Forward propagation
+                current_object = layer.predict(current_object)
+                outputs.append(current_object)
+
+            errors = target[i] - outputs[-1]
+            j = len(self._layers) - 1
+            for layer in self._layers[::-1]: # Backprop
+                if (j == len(self._layers) - 1):
+                    errors = layer.correct_weights(errors, outputs[j], outputs[j - 1])
+                    print(errors)
+                    j -= 1
